2badd66f655991847ec45e204b499bd7777b4b66
aboodsamad <abdelrahman.samad@lau.edu>
2026-09-26 00:16:40 +0300

    fix teh delete and add the edit feature

--- todo-application/src/task-managment/AddTask.tsx
+++ todo-application/src/task-managment/AddTask.tsx
@@ -1,18 +1,33 @@
 import { useState } from "react";
 
 export function AddTask() {
-  const [tasks, setTask] = useState([{ id: 0, task: "", complete: false }]);
+  const [tasks, setTask] = useState<
+    { id: number; task: String; complete: boolean }[]
+  >([]);
   const [text, setText] = useState("");
+  const [edit, setEdit] = useState("");
+  const [editId, setEditId] = useState<null | number>(null);
 
   function addTaskk(e: { preventDefault: () => void }) {
     e.preventDefault();
     if (text.trim() === "") return;
     if (tasks.some((item) => item.task === text)) return;
-    setTask([...tasks, { id: tasks.length, task: text, complete: false }]);
+    console.log(tasks.length);
+    setTask([...tasks, { id: Date.now(), task: text, complete: false }]);
   }
 
   function deleteTask(id) {
     setTask(tasks.filter((item) => item.id !== id));
+  }
+  function saveEdit(id) {
+    if (tasks.some((item) => item.task === edit)) {
+      setEditId(null);
+      return;
+    }
+    setTask(
+      tasks.map((item) => (item.id === id ? { ...item, task: edit } : item)),
+    );
+    setEditId(null);
   }
 
   return (
@@ -21,14 +36,34 @@ export function AddTask() {
         <input value={text} onChange={(e) => setText(e.target.value)} />
         <button type="submit">Add</button>
       </form>
-      <ul>
-        {tasks.map((item) => (
-          <li key={item.id}>
-            {item.task}{" "}
-            <button onClick={() => deleteTask(item.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+      {tasks.length > 0 && (
+        <ul>
+          {tasks.map((item) => (
+            <li key={item.id}>
+              {item.task}{" "}
+              <button onClick={() => deleteTask(item.id)}>Delete</button>
+              <button
+                onClick={() => {
+                  setEditId(item.id);
+                }}
+              >
+                Edit
+              </button>
+              
+              {editId === item.id && (
+                <>
+                  <input
+                    type="text"
+                    value={edit}
+                    onChange={(e) => setEdit(e.target.value)}
+                  />
+                  <button onClick={() => saveEdit(editId)}>Save</button>
+                </>
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
